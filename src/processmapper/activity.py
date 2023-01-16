20c27285f3be3c99cbec56f8ce92caf3b99ee6e5
CS Goh <cs.goh@outlook.com>
2023-01-16 21:30:07 +1300

    Implement skeleton code

--- src/processmapper/activity.py
+++ src/processmapper/activity.py
@@ -1,28 +1,13 @@
-from processmapper.box import Box
+from processmapper.shape import Box
 
 
 class Activity(Box):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.shape_type = "activity"
-        self.shape_color = "white"
-        self.shape_text_color = "black"
-        self.shape_text = name
+    ...
 
 
 class Task(Activity):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.shape_type = "task"
-        self.shape_color = "white"
-        self.shape_text_color = "black"
-        self.shape_text = name
+    ...
 
 
 class Subprocess(Activity):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.shape_type = "subprocess"
-        self.shape_color = "white"
-        self.shape_text_color = "black"
-        self.shape_text = name
+    ...
