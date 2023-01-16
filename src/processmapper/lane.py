41c0eaf422a01677afd62f1a94acfdbbc16a5f79
CS Goh <cs.goh@outlook.com>
2023-01-16 21:31:18 +1300

    Added enum classes and constant variables

--- src/processmapper/lane.py
+++ src/processmapper/lane.py
@@ -1,9 +1,26 @@
 from dataclasses import dataclass, field
-from processmapper.event import Event
-from processmapper.activity import Activity
-from processmapper.gateway import Gateway
+from processmapper.event import Event, Start, End
+from processmapper.activity import Activity, Task, Subprocess
+from processmapper.gateway import Gateway, Exclusive, Parallel, Inclusive
 from processmapper.painter import Painter
-from processmapper.processmap import EventType, ActivityType, GatewayType
+
+
+class EventType:
+    START = "Start"
+    END = "End"
+    TIMER = "Timer"
+    INTERMEDIATE = "Intermediate"
+
+
+class ActivityType:
+    TASK = "Task"
+    SUBPROCESS = "Subprocess"
+
+
+class GatewayType:
+    EXCLUSIVE = "Exclusive"
+    PARALLEL = "Parallel"
+    INCLUSIVE = "Inclusive"
 
 
 @dataclass
@@ -21,7 +38,16 @@ class Lane:
     text_width: int = field(init=False, default=0)
     text_height: int = field(init=False, default=0)
 
+    SURFACE_TOP_MARGIN = 20
+    SURFACE_BOTTOM_MARGIN = 20
+    SURFACE_LEFT_MARGIN = 20
+    SURFACE_RIGHT_MARGIN = 20
+
     LANE_TEXT_SPACE = 40
+    LANE_SHAPE_TOP_MARGIN = 20
+    LANE_SHAPE_BOTTOM_MARGIN = 20
+    LANE_SHAPE_LEFT_MARGIN = 20
+    LANE_SHAPE_RIGHT_MARGIN = 20
 
     def start(self, text: str, type: EventType) -> Event:
         # start = Start(text)
@@ -62,13 +88,11 @@ class Lane:
 
     def set_draw_position(self, x: int, y: int) -> None:
         ### Determine the x and y position of the lane
-        self.x, self.y = x, y
+        self.x = x if x > 0 else self.SURFACE_LEFT_MARGIN
+        self.y = y if y > 0 else self.SURFACE_TOP_MARGIN
 
         if self.shapes:
             for shape in self.shapes:
-                x, y, w, h = shape.set_draw_position(x, y, self.painter)
+                x, y, w, h = shape.set_draw_position(x, y)
                 self.width = max(self.width, x + w)
                 self.height = max(self.height, y + h)
-
-        ### Determine the width and height of the lane
-        pass
