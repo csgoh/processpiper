56532358e62457ec51a66e9f2e8e208b85defe63
CS Goh <cs.goh@outlook.com>
2023-01-21 23:52:35 +1300

    rename lane and added set_colour_palette()

--- src/processmapper/processmap.py
+++ src/processmapper/processmap.py
@@ -5,20 +5,21 @@ from processmapper.painter import Painter
 
 @dataclass
 class ProcessMap:
-    lanes: list = field(init=False, default_factory=list)
+    _lanes: list = field(init=False, default_factory=list)
 
     width: int = field(init=True, default=1200)
     height: int = field(init=True, default=800)
+    colour_theme: str = field(init=True, default="DEFAULT")
 
     def add_lane(self, lane_text: str) -> Lane:
         lane = Lane(lane_text)
-        self.lanes.append(lane)
+        self._lanes.append(lane)
         return lane
 
     def get_surface_size(self) -> tuple:
         x, y = 0, 0
-        if self.lanes:
-            for lane in self.lanes:
+        if self._lanes:
+            for lane in self._lanes:
                 ### Calculate the x and y position of the lane and shapes in the lane
                 x, y, w, h = lane.set_draw_position(x, y, self.__painter)
                 self.width = max(self.width, x + w)
@@ -29,13 +30,19 @@ class ProcessMap:
     def draw(self) -> None:
         self.__painter = Painter(self.width, self.height)
 
+        self.__set_colour_palette(self.colour_theme)
+
         ### Determine the size of the process map
         self.width, self.height = self.get_surface_size()
 
         ### Draw the lanes and the shapes in the lanes
-        if self.lanes:
-            for lane in self.lanes:
+        if self._lanes:
+            for lane in self._lanes:
                 lane.draw()
+
+    def __set_colour_palette(self, palette: str) -> None:
+        """This method sets the colour palette"""
+        # self.__painter.set_colour_palette(palette)
 
     def save(self, filename: str) -> None:
         self.__painter.save_surface(filename)
@@ -47,7 +54,7 @@ class ProcessMap:
         pass
 
     def print(self) -> None:
-        for lane in self.lanes:
+        for lane in self._lanes:
             print(f"[{lane.text}, number of elements: {len(lane.shapes)}]")
             for shape in lane.shapes:
                 print(f'    ("{shape.text}", type: {shape.__class__.__name__})')
