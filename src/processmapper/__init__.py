baaf3f34204cd28a13ea9ec6366b6b06c336baac
CS Goh <cs.goh@outlook.com>
2023-01-31 20:46:15 +1300

    initial version

---
+++ src/processmapper/__init__.py
@@ -0,0 +1,9 @@
+from processmapper.lane import EventType, ActivityType, GatewayType
+from processmapper.processmap import ProcessMap
+
+__all__ = [
+    "EventType",
+    "ActivityType",
+    "GatewayType",
+    "ProcessMap",
+]
