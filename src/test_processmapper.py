8ab3fed8b33796c7467ff95f6a73f187d69f1e07
CS Goh <cs.goh@outlook.com>
2023-02-20 16:00:19 +1300

    change test case

--- src/test_processmapper.py
+++ src/test_processmapper.py
@@ -23,7 +23,7 @@ def test_case1():
 
 
 def test_case2():
-    with ProcessMap(1150, 520) as my_process_map:
+    with ProcessMap(950, 520) as my_process_map:
         with my_process_map.add_lane("Application \nUser") as lane1:
             start = lane1.add_element("Start", EventType.START)
             enter_keyword = lane1.add_element("Enter Keyword", ActivityType.TASK)
@@ -34,13 +34,15 @@ def test_case2():
             search_records = lane2.add_element("Search Records", ActivityType.TASK)
             result_found = lane2.add_element("Result Found?", GatewayType.EXCLUSIVE)
             display_result = lane2.add_element("Display Result", ActivityType.TASK)
+            refine_search = lane2.add_element("Refine Search", ActivityType.TASK)
             logout = lane2.add_element("Logout", ActivityType.TASK)
 
         start.connect(login).connect(enter_keyword).connect(search_records).connect(
             result_found
         )
-        result_found.connect(display_result).connect(logout).connect(end)
-        result_found.connect(search_records)
+        result_found.connect(display_result)
+        display_result.connect(logout).connect(end)
+        result_found.connect(refine_search).connect(search_records)
 
         my_process_map.draw()
         my_process_map.save("my_process_map_test_case02.png")
