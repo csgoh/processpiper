db1ddb9c191e787455eaa1c73179c24af7746468
CS Goh <cs.goh@outlook.com>
2023-02-24 21:26:55 +1300

    added new test cases

--- src/test_processmapper.py
+++ src/test_processmapper.py
@@ -46,6 +46,31 @@ def test_case2():
         my_process_map.save("my_process_map_test_case02.png")
 
 
+def test_case3():
+    with ProcessMap("Test Process", 950, 500) as my_process_map:
+        with my_process_map.add_lane("End User", pool_name="Web Search") as lane1:
+            start = lane1.add_element("Start", EventType.START)
+            enter_keyword = lane1.add_element("Enter Keyword", ActivityType.TASK)
+            end = lane1.add_element("End", EventType.END)
+
+        with my_process_map.add_lane("System", pool_name="Web Search") as lane2:
+            login = lane2.add_element("Login", ActivityType.TASK)
+            search_records = lane2.add_element("Search Records", ActivityType.TASK)
+            result_found = lane2.add_element("Result Found?", GatewayType.EXCLUSIVE)
+            display_result = lane2.add_element("Display Result", ActivityType.TASK)
+            refine_search = lane2.add_element("Refine Search", ActivityType.TASK)
+            logout = lane2.add_element("Logout", ActivityType.TASK)
+
+        start.connect(login).connect(enter_keyword).connect(search_records).connect(
+            result_found
+        ).connect(display_result).connect(logout).connect(end)
+        result_found.connect(refine_search).connect(search_records)
+
+        my_process_map.draw()
+        my_process_map.save("my_process_map_test_case03.png")
+
+
 if __name__ == "__main__":
     # test_case1()
-    test_case2()
+    # test_case2()
+    test_case3()
