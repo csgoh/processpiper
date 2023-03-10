58796233021cda8dbe946235704f9f2dd1670580
CS Goh <cs.goh@outlook.com>
2023-03-10 21:24:30 +1300

    added draw subprocess function

--- src/processmapper/activity.py
+++ src/processmapper/activity.py
@@ -1,4 +1,5 @@
 from processmapper.shape import Box
+from processmapper.painter import Painter
 
 
 class Activity(Box):
@@ -10,4 +11,19 @@ class Task(Activity):
 
 
 class Subprocess(Activity):
-    ...
+    def draw(self, painter: Painter):
+        super().draw(painter)
+        # Draw a plus sign in a box
+
+        subprocess_width, subprocess_height = painter.get_text_dimension(
+            "[+]", "arial.ttf", 14
+        )
+        subprocess_x_pos = self.x + ((self.width - subprocess_width) / 2)
+        painter.draw_text(
+            subprocess_x_pos,
+            self.y + self.height - subprocess_height,
+            "[+]",
+            "arial.ttf",
+            14,
+            "black",
+        )
