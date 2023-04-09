6612033bbb674b50543484df83b01705dff1fa28
CS Goh <cs.goh@outlook.com>
2023-04-09 16:31:33 +1200

    initial version

--- src/processpiper/__init__.py
+++ src/processpiper/__init__.py
@@ -1,5 +1,5 @@
-from processpiper.lane import EventType, ActivityType, GatewayType
-from processpiper.processmap import ProcessMap
+from src.processpiper.lane import EventType, ActivityType, GatewayType
+from src.processpiper.processmap import ProcessMap
 
 __all__ = [
     "EventType",
