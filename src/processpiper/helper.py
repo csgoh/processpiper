6a3c303187468799c906f4218097cccd9830d7d1
CS Goh <cs.goh@outlook.com>
2023-07-22 22:18:04 +1200

    updated switch

--- src/processpiper/helper.py
+++ src/processpiper/helper.py
@@ -24,12 +24,12 @@ from rich.console import Console
 
 
 class Helper:
-    show_layout_grid = True
-    show_pool_lane = True
-    show_x_position = True
-    show_y_position = True
+    show_layout_grid = False
+    show_pool_lane = False
+    show_x_position = False
+    show_y_position = False
     show_draw_position = False
-    show_draw_connection = False
+    show_draw_connection = True
     show_draw = False
     show_general = False
 
