e7b455cca03fde012b4d7f8e63d40bee7e799202
CS Goh <cs.goh@outlook.com>
2023-12-27 17:28:23 +1300

    debug setting

--- src/processpiper/helper.py
+++ src/processpiper/helper.py
@@ -25,13 +25,13 @@ from rich.panel import Panel
 
 
 class Helper:
-    show_layout_grid = False
+    show_layout_grid = True
     show_pool_lane = False
     show_x_position = False
     show_y_position = False
     show_draw_position = True
     show_draw = True
-    show_draw_connection = True
+    show_draw_connection = False
     show_general = False
 
     @staticmethod
