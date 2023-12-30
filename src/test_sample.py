74e5711538c7f64ad11358281cac29ec96a0439d
CS Goh <cs.goh@outlook.com>
2023-12-31 12:55:17 +1300

    added additional test samples

--- src/test_sample.py
+++ src/test_sample.py
@@ -1,4 +1,4 @@
-from .processpiper import ProcessMap, EventType, ActivityType, GatewayType
+from processpiper import ProcessMap, EventType, ActivityType, GatewayType
 
 
 def test_sample01():
@@ -310,10 +310,243 @@ def test_sample06():
         my_process_map.save("images/test/test_sample06.svg")
 
 
+def test_sample07():
+    with ProcessMap(
+        "Product Feature Interface", colour_theme="BLUEMOUNTAIN"
+    ) as my_process_map:
+        with my_process_map.add_lane("system") as lane_system:
+            node_Initiative = lane_system.add_element("Initiative", ActivityType.TASK)
+            node_SI = lane_system.add_element("SI", ActivityType.TASK)
+            node_Feature = lane_system.add_element("Feature", ActivityType.TASK)
+
+            node_Figma_Design = lane_system.add_element(
+                "Figma Design", ActivityType.TASK
+            )
+            node_Story = lane_system.add_element("Story", ActivityType.TASK)
+            node_Product = lane_system.add_element("Product", ActivityType.TASK)
+            node_Bug = lane_system.add_element("Bug", ActivityType.TASK)
+
+            g2 = lane_system.add_element("g2", GatewayType.INCLUSIVE)
+
+            g2.connect(node_Figma_Design)
+            g1 = lane_system.add_element("g1", GatewayType.INCLUSIVE)
+            g1.connect(g2)
+
+            g8 = lane_system.add_element("g8", GatewayType.INCLUSIVE)
+            node_Feature.connect(g8, "")
+
+            g9 = lane_system.add_element("g9", GatewayType.INCLUSIVE)
+            g9.connect(node_Product, "")
+
+        with my_process_map.add_pool("Product What") as pool_Product_What:
+            with pool_Product_What.add_lane("PrSL") as lane_Product_What_PrSL:
+                node_Product_feature_is_d = lane_Product_What_PrSL.add_element(
+                    "Product feature is defined\nwith acceptance Criteria",
+                    ActivityType.TASK,
+                )
+                node_Product_feature_is_d.connect(node_SI, "")
+                node_Initiative.connect(node_Product_feature_is_d, "")
+
+        with pool_Product_What.add_lane("CSB") as lane_Product_What_CSB:
+            node_Product_feature_is_c = lane_Product_What_CSB.add_element(
+                "Product feature is claried in\nbusiness requirements",
+                ActivityType.TASK,
+            )
+            g7 = lane_Product_What_CSB.add_element("g7", GatewayType.INCLUSIVE)
+            node_Product_feature_is_c.connect(g7, "")
+            g7.connect(node_Feature, "")
+            node_SI.connect(node_Product_feature_is_c, "")
+
+        with my_process_map.add_pool("Product How") as pool_Product_How:
+            with pool_Product_How.add_lane("UXD ; CSB") as lane_Product_How_UXD__CSB:
+                node_Collaboratively_deve = lane_Product_How_UXD__CSB.add_element(
+                    "Collaboratively develop UX\nDesigns with CSB", ActivityType.TASK
+                )
+
+                node_Collaboratively_deve.connect(g1, "")
+                g8.connect(node_Collaboratively_deve, "")
+
+            with pool_Product_How.add_lane("CSB ; LRC") as lane_Product_How_CSB__LRC:
+                node_Review_Figma_Design_ = lane_Product_How_CSB__LRC.add_element(
+                    "Review Figma Design with LRC", ActivityType.TASK
+                )
+                node_Review_Figma_Design_.connect(g1, "")
+                node_Figma_Design.connect(node_Review_Figma_Design_, "")
+
+            with pool_Product_How.add_lane("UXD") as lane_Product_How_UXD:
+                node_Update_Figma_Design_ = lane_Product_How_UXD.add_element(
+                    "Update Figma Design based on\nLRC feedback", ActivityType.TASK
+                )
+                node_Update_Figma_Design_.connect(g2, "")
+                g3 = lane_Product_How_UXD.add_element("g3", GatewayType.INCLUSIVE)
+                g3.connect(node_Update_Figma_Design_, "")
+
+            with pool_Product_How.add_lane(
+                "Brokerage Squad; UXD"
+            ) as lane_Product_How_Brokerage_Squad__UXD:
+                node_Work_with_Domains_SL = lane_Product_How_Brokerage_Squad__UXD.add_element(
+                    "Work with Domains SL (Bob\nTaiani, Chris Coale) to\ndevelop features UI stories",
+                    ActivityType.TASK,
+                )
+                node_Work_with_Domains_SL.connect(node_Story, "")
+
+                g3.connect(node_Work_with_Domains_SL)
+
+                node_Figma_Design.connect(g3, "")
+
+            with pool_Product_How.add_lane("CSB") as lane_Product_How_CSB:
+                node_Create_feature_test_ = lane_Product_How_CSB.add_element(
+                    "Create feature test cases", ActivityType.TASK
+                )
+                node_Create_feature_test_.connect(g7, "")
+                g4 = lane_Product_How_CSB.add_element("g4", GatewayType.INCLUSIVE)
+                g8.connect(g4, "")
+                g4.connect(node_Create_feature_test_, "")
+
+            with pool_Product_How.add_lane(
+                "Brokerage Squad"
+            ) as lane_Product_How_Brokerage_Squad:
+                node_Implement_feature_UI = (
+                    lane_Product_How_Brokerage_Squad.add_element(
+                        "Implement feature UI stories", ActivityType.TASK
+                    )
+                )
+                node_Implement_feature_UI.connect(g9, "")
+                node_Story.connect(node_Implement_feature_UI, "")
+                g5 = lane_Product_How_Brokerage_Squad.add_element(
+                    "g5", GatewayType.INCLUSIVE
+                )
+                g2.connect(g5)
+                g5.connect(node_Implement_feature_UI, "")
+                node_Verify_UI_in_virtual = lane_Product_How_UXD.add_element(
+                    "Verify UI in virtual ui test\nenvironment implemented as\nspeced in Figma Design",
+                    ActivityType.TASK,
+                )
+                g5.connect(node_Verify_UI_in_virtual, "")
+                node_Verify_UI_in_virtual.connect(node_Bug, "")
+                node_Product.connect(node_Verify_UI_in_virtual, "")
+                node_Modify_screens_based = (
+                    lane_Product_How_Brokerage_Squad.add_element(
+                        "Modify screens based on UXD\nfeedback", ActivityType.TASK
+                    )
+                )
+                node_Modify_screens_based.connect(g9, "")
+                g6 = lane_Product_How_Brokerage_Squad.add_element(
+                    "g6", GatewayType.INCLUSIVE
+                )
+                g6.connect(node_Modify_screens_based, "")
+                node_Bug.connect(g6, "")
+                node_Product.connect(g6, "")
+                node_Validate_that_Produc = lane_Product_How_CSB.add_element(
+                    "Validate that Product meets\ncustomer needs per spec",
+                    ActivityType.TASK,
+                )
+                node_Validate_that_Produc.connect(node_Bug, "")
+                node_Feature.connect(node_Validate_that_Produc, "")
+                node_Modify_screens_based = (
+                    lane_Product_How_Brokerage_Squad.add_element(
+                        "Modify screens based on CSB\nfeedback", ActivityType.TASK
+                    )
+                )
+                node_Modify_screens_based.connect(node_Product, "")
+                node_Bug.connect(node_Modify_screens_based, "")
+
+        my_process_map.draw()
+        my_process_map.save("images/test/test_sample07.png")
+
+
+def test_sample08():
+    with ProcessMap(
+        "Test Sample 08", colour_theme="TEALWATERS"
+    ) as my_process_map:
+        with my_process_map.add_lane("Lane 1") as lane1:
+            start = lane1.add_element("start", EventType.START)
+            t11 = lane1.add_element("Task 1-1", ActivityType.TASK)
+            start.connect(t11)
+        with my_process_map.add_pool("Pool 1") as pool1:
+            with pool1.add_lane("Lane 2") as lane2:
+                t21 = lane2.add_element("Task 2-1", ActivityType.TASK)
+                g22 = lane2.add_element("Gateway 2-2", GatewayType.EXCLUSIVE)
+                t23 = lane2.add_element("Task 2-3", ActivityType.TASK)
+                t24 = lane2.add_element("Task 2-4", ActivityType.TASK)
+                t11.connect(t21)
+                t21.connect(g22)
+                g22.connect(t21, "Yes")
+                g22.connect(t23, "No")
+            with pool1.add_lane("Lane 3") as lane3:
+                g31 = lane3.add_element("Gateway 3-1", GatewayType.EXCLUSIVE)
+                end = lane3.add_element("end", EventType.END)
+                t23.connect(g31)
+                g31.connect(t11, "Yes")
+                g31.connect(t24, "No")
+                t24.connect(end)
+
+        my_process_map.draw()
+        my_process_map.save("images/test/test_sample08.png")
+
+
+def test_sample09():
+    with ProcessMap(
+        "Test Sample 09", colour_theme="TEALWATERS") as my_process_map:
+        with my_process_map.add_lane("Lane 1") as lane1:
+            start = lane1.add_element("start", EventType.START)
+            t11 = lane1.add_element("Task 1-1", ActivityType.TASK)
+            start.connect(t11)
+        with my_process_map.add_pool("Pool 1") as pool1:
+            with pool1.add_lane("Lane 2") as lane2:
+                t21 = lane2.add_element("Task 2-1", ActivityType.TASK)
+                g22 = lane2.add_element("Gateway 2-2", GatewayType.EXCLUSIVE)
+                t23 = lane2.add_element("Task 2-3", ActivityType.TASK)
+                t24 = lane2.add_element("Task 2-4", ActivityType.TASK)
+                t11.connect(t21)
+                t21.connect(g22)
+                g22.connect(t21, "Yes")
+                g22.connect(t23, "No")
+            with pool1.add_lane("Lane 3") as lane3:
+                end = lane3.add_element("end", EventType.END)
+                t23.connect(end)
+                t24.connect(end)
+            
+        my_process_map.draw()
+        my_process_map.save("images/test/test_sample09.png")
+
+
+def test_sample10():
+    with ProcessMap(
+        "Test Sample 10", colour_theme="RUBYRED") as my_process_map:
+        with my_process_map.add_lane("Lane 1") as lane1:
+            start = lane1.add_element("start", EventType.START)
+            t11 = lane1.add_element("Task 1-1", ActivityType.TASK)
+            start.connect(t11)
+        with my_process_map.add_pool("Pool 1") as pool1:
+            with pool1.add_lane("Lane 2") as lane2:
+                t21 = lane2.add_element("Task 2-1", ActivityType.TASK)
+                g22 = lane2.add_element("Gateway 2-2", GatewayType.EXCLUSIVE)
+                t23 = lane2.add_element("Task 2-3", ActivityType.TASK)
+                t24 = lane2.add_element("Task 2-4", ActivityType.TASK)
+                t11.connect(t21)
+                t21.connect(g22)
+                g22.connect(t21, "Yes")
+                g22.connect(t23, "No")
+                t23.connect(t24)
+            with pool1.add_lane("Lane 3") as lane3:
+                g31 = lane3.add_element("Gateway 3-1", GatewayType.EXCLUSIVE)
+                end = lane3.add_element("end", EventType.END)
+                g31.connect(t11, "Yes")
+                t24.connect(end)
+
+        my_process_map.draw()
+        my_process_map.save("images/test/test_sample10.png")
+
+
 if __name__ == "__main__":
-    test_sample01()
-    test_sample02()
-    test_sample03()
-    #test_sample04()  # -- Test validation
-    #test_sample05()  # -- Test validation
-    #test_sample06()  # -- Test validation
+    # test_sample01()
+    # test_sample02()
+    # test_sample03()
+    # test_sample04()  # -- Test validation. Should fail
+    # test_sample05()  # -- Test validation. Should fail
+    # test_sample06()  # -- Test validation. Should fail
+    # test_sample07()
+    test_sample08()
+    # test_sample09()  # -- Test validation. Should fail
+    # test_sample10()  # -- Test validation. Should fail
